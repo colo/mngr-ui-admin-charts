5fa0db4a58cf08204d2b8a76a5c60575f69023df
Luciano Bueno <glamero@gmail.com>
2018-12-27 18:19:15 -0300

    bump

--- os/procs_top.js
+++ os/procs_top.js
@@ -93,11 +93,17 @@ let debug_internals = require('debug')('mngr-ui-admin-charts:os:procs_top:Intern
 
 let DefaultDygraphLine = require('../defaults/dygraph.line')
 
+let allowed_names = /cpu|mem|elapsed|time|count/
 module.exports = Object.merge(Object.clone(DefaultDygraphLine),{
   pre_process: function(chart, name, stat){
-    debug_internals('os_procs_stats pre_process %o %s %o', chart, name, stat)
-
+    debug_internals('pre_process %s %o', name, stat)
+    // chart.name = name
+    // if(allowed_names.test(name)){
     return chart
+    // }
+    // else{
+    //   return undefined
+    // }
   },
   top: {
     count: 5,
@@ -109,73 +115,79 @@ module.exports = Object.merge(Object.clone(DefaultDygraphLine),{
     labels: ['Time'],
   },
   // "options": undefined,
-  match: /^os_procs_stats$/,
+  // match: /^os_procs_stats$/,
   // match: /^[a-zA-Z0-9_]*$/,
   watch: {
 
-    value: undefined,
+    // value: undefined,
     transform: function(values, caller, chart, cb){
-      debug_internals('os_procs_stats_percentage_mem transform %o', values)
+      // // debug_internals('transform %o %s', values, chart.matched)
+      if(allowed_names.test(chart.matched)){
 
-      // //console.log('os_procs_stats_percentage_mem transform', values)
-      let transformed = []
+        // debug_internals('transform %o %s', values, chart.matched)
 
-      for(let index = 0; index < values.length; index++){
-        // let transform_value = []
-        let val = values[index]
+        // //console.log('os_procs_stats_percentage_mem transform', values)
+        let transformed = []
+
+        for(let index = 0; index < values.length; index++){
+          // let transform_value = []
+          let val = values[index]
 
 
-        // let length = val.value.length
-        // val.value.splice(
-        //   (chart.top * -1) -1,
-        //   length - chart.top
-        // )
-        //
-        // self.$set(self.available_charts[self.host+'.os_procs_stats_percentage_mem'].chart.options, 'labels', ['Time'])
-        //
-        if(chart.options.labels.length == 1){//process for the first time only, if you wanna re process, you need to reload
+          // let length = val.value.length
+          // val.value.splice(
+          //   (chart.top * -1) -1,
+          //   length - chart.top
+          // )
+          //
+          // self.$set(self.available_charts[self.host+'.os_procs_stats_percentage_mem'].chart.options, 'labels', ['Time'])
+          //
+          if(chart.options.labels.length == 1){//process for the first time only, if you wanna re process, you need to reload
+            Array.each(val.value, function(data, data_index){
+              // //console.log('pre transformed: ', data)
+              // if(index < chart.top)
+              //   transform_value.push(data['pid']*1)
+
+              if(chart.options.labels.length < chart.top.count){
+                // chart.options.labels[index + 1] = 'pid['+data.pid+']'
+                chart.options.labels.push('pid['+data['pid']+']')
+                chart.top.pids.push(data['pid'])
+              }
+
+            })
+
+            chart.options.labels.push('pid[others]')
+          }
+
+          let transform_value = new Array(chart.options.labels.length - 1)
+          transform_value.fill(0)
+
+
+          let _others_index = chart.options.labels.length - 2 //remember, first label is Time
           Array.each(val.value, function(data, data_index){
-            // //console.log('pre transformed: ', data)
-            // if(index < chart.top)
-            //   transform_value.push(data['pid']*1)
-
-            if(chart.options.labels.length < chart.top.count){
-              // chart.options.labels[index + 1] = 'pid['+data.pid+']'
-              chart.options.labels.push('pid['+data['pid']+']')
-              chart.top.pids.push(data['pid'])
+            let _index = chart.top.pids.indexOf(data['pid'])
+            if(_index > -1){
+              transform_value[_index] = data[chart.matched]
             }
-
+            else{
+              transform_value[_others_index] = (!transform_value[_others_index]) ? data[chart.matched] : transform_value[_others_index] + data[chart.matched]
+            }
           })
 
-          chart.options.labels.push('pid[others]')
+          if(!transform_value[_others_index])
+            transform_value[_others_index] = 0
+
+          let transform = {timestamp: val.timestamp, value: transform_value}
+
+          transformed.push(transform)
+
+          if(index == values.length -1){
+            // //console.log('transformed: ', transformed)
+            debug_internals('transform %o %s', transformed, chart.matched)
+            cb( transformed )
+          }
         }
 
-        let transform_value = new Array(chart.options.labels.length - 1)
-        transform_value.fill(0)
-
-
-        let _others_index = chart.options.labels.length - 2 //remember, first label is Time
-        Array.each(val.value, function(data, data_index){
-          let _index = chart.top.pids.indexOf(data['pid'])
-          if(_index > -1){
-            transform_value[_index] = data['%mem']
-          }
-          else{
-            transform_value[_others_index] = (!transform_value[_others_index]) ? data['%mem'] : transform_value[_others_index] + data['%mem']
-          }
-        })
-
-        if(!transform_value[_others_index])
-          transform_value[_others_index] = 0
-
-        let transform = {timestamp: val.timestamp, value: transform_value}
-
-        transformed.push(transform)
-
-        if(index == values.length -1){
-          // //console.log('transformed: ', transformed)
-          cb( transformed )
-        }
       }
     }
   }
