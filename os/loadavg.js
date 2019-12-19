465e264d60fcd962a215a2310761f5d92bce0528
Luciano Bueno <glamero@gmail.com>
2019-12-19 18:24:24 -0300

    #0000 - bump

--- os/loadavg.js
+++ os/loadavg.js
@@ -3,12 +3,12 @@ let DefaultDygraphLine = require('../defaults/dygraph.line')
 module.exports = Object.merge(Object.clone(DefaultDygraphLine),{
 	"interval": 5,
   "skip": 5,
-  
-  pre_process: function(chart, name, stat){
-    return chart
-  },
+
+  // pre_process: function(chart, name, stat){
+  //   return chart
+  // },
   "options": {
-    labels: ['Time', '1 min avg', '5 min avg', '15 min avg'],
+    // labels: ['Time', '1 min avg', '5 min avg', '15 min avg'],
     fillGraph: false,
   }
 })
