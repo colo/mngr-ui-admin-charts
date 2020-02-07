17c07f72415efa6bfb0afe8352656604a0d08f43
Luciano Bueno <glamero@gmail.com>
2020-02-07 13:18:22 -0300

    #0000 - Moved cpus.io as first label

--- os/cpus.tabular.js
+++ os/cpus.tabular.js
@@ -34,7 +34,7 @@ module.exports = Object.merge(Object.clone(DefaultDygraphLine),{
               //decrease index becasue we won't have 'cores' in graphs
 
               let __val = (col - chart.prev[index]) / ((row[0] - chart.prev[0]) / 1000) //DERIVE
-              new_row[index - 1] = (__val > (chart.cores * 10000 )) ? __val / 2 : __val
+              new_row[index] = (__val > (chart.cores * 10000 )) ? __val / 2 : __val
 
               // row[index] = ((index === 5 || index === 6) && __val > 20000) ? __val / 2 : __val
               // row[index] = ((index === 5 || index === 6) && row[index] > 20000) ? -1 : row[index]
@@ -42,13 +42,14 @@ module.exports = Object.merge(Object.clone(DefaultDygraphLine),{
 
               // new_row[index - 1] = col - chart.prev[index]
               // new_row[index - 1] = (col - chart.prev[index]) / ((row[0] - chart.prev[0]) / 1000) //DERIVE
-              sum += new_row[index - 1]
+              sum += new_row[index]
 
 
             }
           })
           let _io = (chart.cores * 10000 ) - sum
-          new_row.push((_io < 0) ? 0 : _io)
+          // new_row.push((_io < 0) ? 0 : _io)
+          new_row[1] = (_io < 0) ? 0 : _io
           // if(sum > (chart.cores * 10000 )){
           //   values[row_index] = undefined
           // }
@@ -94,7 +95,7 @@ module.exports = Object.merge(Object.clone(DefaultDygraphLine),{
   },
 
   options: {
-    labels: ['Time', 'idle', 'irq', 'nice', 'sys', 'user', 'io'],
+    labels: ['Time', 'io', 'idle', 'irq', 'nice', 'sys', 'user' ],
     stackedGraph: true,
     valueRange: [0]
   }
